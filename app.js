66e640e1d5365cc2b3040260b9448cef7c87a7a4
SK02K1 <ksourabh458@gmail.com>
2021-06-19 01:20:43 +0530

    btn wiring done

---
+++ app.js
@@ -0,0 +1,8 @@
+const inputContainer = document.querySelector(".input-container");
+const btnTranslate = document.querySelector(".btn-translate");
+
+const btnClickHandler = () =>{
+    console.log(inputContainer.value);
+}
+
+btnTranslate.addEventListener("click", btnClickHandler);
