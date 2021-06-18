2a563bef53a40f6ee6801174cdce83f93c0385af
SK02K1 <ksourabh458@gmail.com>
2021-06-19 01:40:32 +0530

    feat : english to minion added

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const inputContainer = document.querySelector(".input-container");
 const btnTranslate = document.querySelector(".btn-translate");
 const outputContainer = document.querySelector(".output-container");
+const url = "https://api.funtranslations.com/translate/minion.json";
 
+const urlConstructor = text => url + "?text=" + text;
 
 const btnClickHandler = () => {
-    outputContainer.innerText = inputContainer.value;
+    fetch(urlConstructor(inputContainer.value))
+    .then(Response => Response.json())
+    .then(json => outputContainer.innerText = json.contents.translated)
 }
 
 btnTranslate.addEventListener("click", btnClickHandler);
+
+
