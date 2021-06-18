97b43fac318e3c0b4b936bb569c04f60528f63b4
SK02K1 <ksourabh458@gmail.com>
2021-06-19 01:43:58 +0530

    feat : error handler added

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const outputContainer = document.querySelector(".output-container");
 const url = "https://api.funtranslations.com/translate/minion.json";
 
 const urlConstructor = text => url + "?text=" + text;
+const errorHandler = error => alert(`${error}, please try after some time!`);
 
 const btnClickHandler = () => {
     fetch(urlConstructor(inputContainer.value))
     .then(Response => Response.json())
     .then(json => outputContainer.innerText = json.contents.translated)
+    .catch(errorHandler)
 }
 
 btnTranslate.addEventListener("click", btnClickHandler);
